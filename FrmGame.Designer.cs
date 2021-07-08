c179f9be99e12742e21125b38e64dd0ab9717ab4
Amy Burgess <amyburgess@mmc.school.nz>
2021-07-08 13:22:14 +1200

    started labels 8/07

--- FrmGame.Designer.cs
+++ FrmGame.Designer.cs
@@ -32,6 +32,9 @@ namespace _2021_game
             this.components = new System.ComponentModel.Container();
             this.PnlGame = new System.Windows.Forms.Panel();
             this.TmrCupcake = new System.Windows.Forms.Timer(this.components);
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // PnlGame
@@ -49,16 +52,45 @@ namespace _2021_game
             this.TmrCupcake.Enabled = true;
             this.TmrCupcake.Tick += new System.EventHandler(this.TmrCupcake_Tick);
             // 
+            // TxtName
+            // 
+            this.TxtName.Location = new System.Drawing.Point(170, 12);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(100, 20);
+            this.TxtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Score";
+            // 
             // FrmGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtName);
             this.Controls.Add(this.PnlGame);
             this.Name = "FrmGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -66,6 +98,9 @@ namespace _2021_game
 
         private System.Windows.Forms.Panel PnlGame;
         private System.Windows.Forms.Timer TmrCupcake;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 

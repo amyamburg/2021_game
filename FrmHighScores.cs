b80530654168422ffd57316bc0a1fdbb12bcfce7
Amy Burgess <amyburgess@mmc.school.nz>
2021-08-03 09:30:42 +1200

    commented on FrmHighScores

--- FrmHighScores.cs
+++ FrmHighScores.cs
@@ -42,6 +42,7 @@ namespace _2021_game
         {
             foreach (HighScores s in highScores)
             {
+                //list 10 high score names and scores in their boxes
                 lstBoxName.Items.Add(s.Name);
                 lstBoxScore.Items.Add(s.Score);
 
@@ -52,27 +53,29 @@ namespace _2021_game
         {
             int lowest_score = highScores[(highScores.Count - 1)].Score;
             if (int.Parse(LblPlayerScore.Text) > lowest_score)
-            {
+            { //if the user gets a score that is higher than the lowest score, tell them they made it to the top 10.
                 lblMessage.Text = "You have made the Top Ten! Well Done!";
+                //add their name and score as a high score
                 highScores.Add(new HighScores(LblPlayerName.Text, int.Parse(LblPlayerScore.Text)));
 
             }
             else
             {
+                //tell the user that they did not get enough points to get on the scoreboard
                 lblMessage.Text = "Keep trying to make the top ten!";
             }
-
+            
             SortHighScores();
 
             DisplayHighScores();
 
         }
         public void SortHighScores()
-        {
+        {  //put the high scores in order of highest to lowest, and only use the top 10
             highScores = highScores.OrderByDescending(hs => hs.Score).Take(10).ToList();
         }
 
-        public void SaveHighScores()
+        public void SaveHighScores() //add users' high score to highscores so it will show next time
         {
             StringBuilder builder = new StringBuilder();
             foreach (HighScores score in highScores)
@@ -86,7 +89,7 @@ namespace _2021_game
         private void BtnReturn_Click(object sender, EventArgs e)
         {
             SaveHighScores();
-
+            //start the game form up and hide the high scores form
             FrmGame FrmGame2 = new FrmGame();
             Hide();
             FrmGame2.ShowDialog();
